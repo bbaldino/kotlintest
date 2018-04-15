8ac599166192d44ec129a50b4f9f0366c67a4bbd
brian baldino <bbaldino@gmail.com>
2018-04-14 23:27:39 -0700

    wip: seems to be working

--- kotlintest-core/src/main/kotlin/io/kotlintest/specs/AbstractShouldSpec.kt
+++ kotlintest-core/src/main/kotlin/io/kotlintest/specs/AbstractShouldSpec.kt
@@ -23,15 +23,19 @@ import io.kotlintest.lineNumber
  *    // test here
  *  }
  */
+
+data class WithContext(val name: String, val ctx: () -> Unit)
+
 abstract class AbstractShouldSpec(body: AbstractShouldSpec.() -> Unit = {}) : AbstractSpec() {
 
   init {
     body()
   }
 
-  final override fun isInstancePerTest(): Boolean = true
+  final override fun isInstancePerTest(): Boolean = false
 
-  private val stateStack = mutableListOf<() -> Unit>()
+//  private val stateStack = mutableListOf<() -> Unit>()
+  private val stateStack = mutableListOf<WithContext>()
 
   fun should(name: String, test: TestContext.() -> Unit): TestCase {
     val tc = TestCase(rootDescription().append("should $name"), this@AbstractShouldSpec, test, lineNumber(), defaultTestCaseConfig)
@@ -39,29 +43,52 @@ abstract class AbstractShouldSpec(body: AbstractShouldSpec.() -> Unit = {}) : Ab
     return tc
   }
 
-  fun withContext(ctx: () -> Unit) {
-    println("Adding context to ${this@AbstractShouldSpec}")
+//  fun withContext(name: String, ctx: () -> Unit) {
+//    println("Adding context $name to ${this@AbstractShouldSpec}")
+//    stateStack.add(ctx)
+//  }
+
+  fun withContext(ctx: WithContext) {
+    println("AbstractShouldSpec ${this@AbstractShouldSpec} adding withcontext ${ctx.name}")
     stateStack.add(ctx)
   }
 
-  operator fun String.invoke(init: ShouldContext.() -> Unit) =
-      rootScopes.add(TestContainer(rootDescription().append(this), this@AbstractShouldSpec, { ShouldContext(it, stateStack).init() }))
+  operator fun String.invoke(init: ShouldContext.() -> Unit) {
+    println("AbstractShouldSpec Invoking string from ${this@AbstractShouldSpec}, root description is: ${rootDescription().name}")
+    rootScopes.add(TestContainer(rootDescription().append(this), this@AbstractShouldSpec, { ShouldContext(it, stateStack).init() }))
+  }
 
-  inner class ShouldContext(val context: TestContext, inheritedStateStack: List<() -> Unit> = listOf()) {
+
+//  inner class ShouldContext(val context: TestContext, inheritedStateStack: List<() -> Unit> = listOf()) {
+  inner class ShouldContext(val context: TestContext, inheritedStateStack: List<WithContext> = listOf()) {
+
+    init {
+      println("Creating ShouldContext with ${inheritedStateStack.size} WithContexts:")
+      inheritedStateStack.forEach { println(it) }
+    }
 
     private val stateStack = inheritedStateStack.toMutableList()
 
-    fun withContext(ctx: () -> Unit) {
-      println("Adding context to ${this@ShouldContext}")
+//    fun withContext(ctx: () -> Unit) {
+    fun withContext(ctx: WithContext) {
+      println("ShouldContext ${this@ShouldContext} adding withcontext ${ctx.name}")
       stateStack.add(ctx)
     }
 
-    operator fun String.invoke(init: ShouldContext.() -> Unit) =
-        context.addScope(TestContainer(context.currentScope().description().append(this), this@AbstractShouldSpec, { ShouldContext(it).init() }))
+    operator fun String.invoke(init: ShouldContext.() -> Unit) {
+//      val testWrapper = { context: TestContext ->
+//        stateStack.forEach { it.ctx() }
+//        ShouldContext(context).init()
+//      }
+      context.addScope(TestContainer(context.currentScope().description().append(this), this@AbstractShouldSpec, { ShouldContext(it, stateStack).init() }))
+      //context.addScope(TestContainer(context.currentScope().description().append(this), this@AbstractShouldSpec, testWrapper))
+    }
+
 
     fun should(name: String, test: TestContext.() -> Unit): TestCase {
       val testWrapper = { context: TestContext ->
-        stateStack.forEach { it() }
+        println("ShouldContext test $name has ${stateStack.size} contexts to execute")
+        stateStack.forEach { it.ctx() }
         test(context)
       }
       val tc = TestCase(context.currentScope().description().append("should $name"), this@AbstractShouldSpec, testWrapper, lineNumber(), defaultTestCaseConfig)
@@ -69,6 +96,4 @@ abstract class AbstractShouldSpec(body: AbstractShouldSpec.() -> Unit = {}) : Ab
       return tc
     }
   }
-
-
 }
--- kotlintest-tests/src/test/kotlin/io/kotlintest/specs/ShouldSpecTest.kt
+++ kotlintest-tests/src/test/kotlin/io/kotlintest/specs/ShouldSpecTest.kt
@@ -2,6 +2,7 @@ package io.kotlintest.specs
 
 import io.kotlintest.Description
 import io.kotlintest.ListStack
+import io.kotlintest.Spec
 import io.kotlintest.matchers.gt
 import io.kotlintest.matchers.lt
 import io.kotlintest.shouldBe
@@ -9,98 +10,148 @@ import io.kotlintest.shouldBe
 class ShouldSpecTest : AbstractShouldSpec() {
   private var num = -1
 
+  override fun beforeSpec(description: Description, spec: Spec) {
+    super.beforeSpec(description, spec)
+    println("======>MY TEST")
+  }
+
   override fun beforeTest(description: Description) {
     super.beforeTest(description)
   }
 
   init {
     // My tests
-//    "When you start with 0" {
-//      println("Adding 'setting num to 0' context")
-//      withContext {
-//        println("setting num to 0")
+    println("declaring top level context in test")
+    "When you start with 0" {
+      println("Adding 'setting num to 0' context")
+//      withContext("setting num to 0") {
+//        println("WC: setting num to 0")
 //        num = 0
 //      }
-//      "and add 10" {
-//        withContext {
-//          println("adding 10")
+      withContext(WithContext("setting num to 0") {
+        println("WC: setting num to 0")
+        num = 0
+      })
+      "and add 10" {
+//        withContext("adding 10") {
+//          println("WC: adding 10")
 //          num += 10
 //        }
-//        "and then subtract 5" {
-//            withContext {
-//              println("subtracting 5")
+        withContext(WithContext("adding 10") {
+          println("WC: adding 10")
+          num += 10
+        })
+        "and then subtract 5" {
+//            withContext("subtracting 5") {
+//              println("WC: subtracting 5")
 //                num -= 5
 //            }
-//            should("be equal to 5") {
-//              println("testing for 5")
-//                num shouldBe 5
-//            }
-//        }
-//        should("be equal to 10") {
-//          num shouldBe 10
-//        }
-//        "and then add 5" {
-//          withContext {
+          withContext(WithContext("subtracting 5") {
+            println("WC: subtracing 5")
+            num -= 5
+          })
+            should("be equal to 5") {
+              println("testing for 5")
+              num shouldBe 5
+            }
+        }
+        should("be equal to 10") {
+          num shouldBe 10
+        }
+        "and then add 5" {
+//          withContext("adding 5") {
+//            println("WC: adding 5")
 //            num += 5
 //          }
-//          should("be equal to 15") {
-//            num shouldBe 15
-//          }
-//        }
-//
+          withContext(WithContext("adding 5") {
+            println("WC: adding 5")
+            num += 5
+          })
+          should("be equal to 15") {
+            num shouldBe 15
+          }
+        }
+      }
+
+    }
+
+//    println("Test Init")
+//    var count = 0
+//    "when count is increased by 10" {
+//      count += 10
+//      should("be 10") {
+//        count shouldBe 10
 //      }
-//
+//    }
+//    should("be 0") {
+//      count shouldBe 0
+//      count = 100
+//    }
+//    "when count is increased by 20" {
+//      count += 10
+//      should("be 20") {
+//        count shouldBe 20
+//      }
+//    }
+//    should("be 0 part 2") {
+//      count shouldBe 0
+//      count = 100
+//    }
+//    should("be 0 part 3") {
+//      count shouldBe 0
+//      count = 100
+//    }
+//    should("still be 0") {
+//      count shouldBe 0
 //    }
 
 
 
-
-
     // should allow multi nested
-    "List" {
-      "pop" {
-        should("remove the last element from stack") {
-          val stack = ListStack<String>()
-          stack.push("hello")
-          stack.push("world")
-          stack.size() shouldBe 2
-          stack.pop() shouldBe "world"
-          stack.size() shouldBe 1
-        }
-      }
-      should("remove the last element from stack") {
-        val stack = ListStack<String>()
-        stack.push("hello")
-        stack.push("world")
-        stack.size() shouldBe 2
-        stack.pop() shouldBe "world"
-        stack.size() shouldBe 1
-      }
-    }
-
-    // should allow nested
-    "List.pop" {
-      should("remove the last element from stack") {
-        val stack = ListStack<String>()
-        stack.push("hello")
-        stack.push("world")
-        stack.size() shouldBe 2
-        stack.pop() shouldBe "world"
-        stack.size() shouldBe 1
-      }
-    }
-
-    // and un-nested
-    should("leave the stack unmodified") {
-      val stack = ListStack<String>()
-      stack.push("hello")
-      stack.push("world")
-      stack.size() shouldBe 2
-      stack.peek() shouldBe "world"
-      stack.size() shouldBe 2
-    }
-
-    should("support config") {
-    }.config(invocations = 5)
+//    "List" {
+//      "pop" {
+//        should("remove the last element from stack") {
+//          val stack = ListStack<String>()
+//          stack.push("hello")
+//          stack.push("world")
+//          stack.size() shouldBe 2
+//          stack.pop() shouldBe "world"
+//          stack.size() shouldBe 1
+//        }
+//      }
+//      should("remove the last element from stack") {
+//        val stack = ListStack<String>()
+//        stack.push("hello")
+//        stack.push("world")
+//        stack.size() shouldBe 2
+//        stack.pop() shouldBe "world"
+//        stack.size() shouldBe 1
+//      }
+//    }
+//
+//    // should allow nested
+//    "List.pop" {
+//      should("remove the last element from stack") {
+//        val stack = ListStack<String>()
+//        stack.push("hello")
+//        stack.push("world")
+//        stack.size() shouldBe 2
+//        stack.pop() shouldBe "world"
+//        stack.size() shouldBe 1
+//      }
+//    }
+//
+//    // and un-nested
+//    should("leave the stack unmodified") {
+//      val stack = ListStack<String>()
+//      stack.push("hello")
+//      stack.push("world")
+//      stack.size() shouldBe 2
+//      stack.peek() shouldBe "world"
+//      stack.size() shouldBe 2
+//    }
+//
+//    should("support config") {
+//    }.config(invocations = 5)
   }
 }
